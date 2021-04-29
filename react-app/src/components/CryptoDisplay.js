49d9c2e074d35018146377869bfd7b604172c59e
David <dyclee@umich.edu>
2021-04-28 22:53:10 -0500

    styling blocks

--- react-app/src/components/CryptoDisplay.js
+++ react-app/src/components/CryptoDisplay.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const CryptoDisplay = ({crypto}) => {
     let roundPrice = (crypto.quote.USD.price).toFixed(2);
+    let hourChange = (crypto.quote.USD.percent_change_1h).toFixed(2);
     return (<>
         <div className="currency-container">
             <div className="currency-header">
@@ -14,6 +15,15 @@ export const CryptoDisplay = ({crypto}) => {
                 <div className="currency-quote">
                     $ {roundPrice}
                 </div>
+                {hourChange >= 0 ?
+                <div className="currency-change-plus">
+                    $ {hourChange}
+                </div>
+                :
+                <div className="currency-change-minus">
+                    $ {hourChange}
+                </div>
+                }
             </div>
         </div>
     </>)
